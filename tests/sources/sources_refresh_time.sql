f60bd403729c00683c5ee52e35137676da7a5efa
Edward King <ed.king@truereligion.com>
2025-03-04 16:05:10 -0500

    add sources_refresh_time

---
+++ tests/sources/sources_refresh_time.sql
@@ -0,0 +1,18 @@
+with
+cte_test as (
+    select
+        '{{this.name}}' as test_name,
+        null as source_name,
+        null as dimension,
+        'Validation freshness' as description,
+        null as source_synced_ts,
+        null as max_data_date,
+        analysis.local_time(current_timestamp()) as tested_ts,
+        null as data,
+        null as stop_execution,
+        tested_ts > current_date() + 1 as passed
+)
+select
+    *
+from cte_test
+where passed = false
