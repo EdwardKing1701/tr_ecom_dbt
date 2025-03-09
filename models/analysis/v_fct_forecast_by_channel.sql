de446423388626a90b20a06db691e584ea2509d7
Edward King <ed.king@truereligion.com>
2025-03-09 15:02:23 -0400

    update v_fct_forecast_by_day

--- models/analysis/v_fct_forecast_by_channel.sql
+++ models/analysis/v_fct_forecast_by_channel.sql
@@ -3,6 +3,14 @@
         materialized = 'view'
     )
 }}
+with
+cte_last_updated as (
+    select
+        day_key,
+        max(rcd_upd_ts) as rcd_upd_ts
+    from {{source('robling_tr', 'dwh_f_web_pln_d_b')}}
+    group by all
+)
 select
     day_key as date,
     case
@@ -14,5 +22,6 @@ select
     f_web_bdgt_co_ord_rtl as budget,
     rcd_upd_ts as source_synced_ts
 from {{source('robling_tr', 'dwh_f_web_pln_d_b')}}
+natural join cte_last_updated
 where
     channel <> 'ECOM Total'
--- models/analysis/v_fct_forecast_by_day.sql
+++ models/analysis/v_fct_forecast_by_day.sql
@@ -3,17 +3,26 @@
         materialized = 'view'
     )
 }}
+with
+cte_last_updated as (
+    select
+        day_key,
+        max(rcd_upd_ts) as rcd_upd_ts
+    from {{source('robling_tr', 'dwh_f_web_pln_d_b')}}
+    group by all
+)
 select
     day_key as date,
-    f_web_fcst_co_ord_cnt as orders_forecast,
-    f_web_fcst_co_ord_qty as sale_qty_forecast,
+    f_web_fcst_co_ord_qty as orders_forecast,
+    f_web_fcst_co_ord_cnt as sale_qty_forecast,
     f_web_fcst_co_ord_rtl as sale_amt_forecast,
     f_web_fcst_trfc_cnt as sessions_forecast,
-    f_web_bdgt_co_ord_cnt as orders_budget,
-    f_web_bdgt_co_ord_qty as sale_qty_budget,
+    f_web_bdgt_co_ord_qty as orders_budget,
+    f_web_bdgt_co_ord_cnt as sale_qty_budget,
     f_web_bdgt_co_ord_rtl as sale_amt_budget,
     f_web_bdgt_trfc_cnt as sessions_budget,
     rcd_upd_ts as source_synced_ts
 from {{source('robling_tr', 'dwh_f_web_pln_d_b')}}
+natural join cte_last_updated
 where
     channel = 'ECOM Total'
