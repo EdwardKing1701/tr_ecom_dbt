358a844463188ad940364e5f0e50b049e13ca83c
Edward King <ed.king@truereligion.com>
2025-03-11 10:28:30 -0400

    update fct_spend_by_channel

--- models/analysis/daily/fct_spend_by_channel.sql
+++ models/analysis/daily/fct_spend_by_channel.sql
@@ -4,9 +4,26 @@
         pk = ['date', 'channel']
     )
 }}
+with
+cte_spend as (
+    select
+        date,
+        channel,
+        spend
+    from {{ref('marketing_spend')}}
+),
+cte_forecast as (
+    select
+        date,
+        channel,
+        spend_forecast
+    from {{ref('marketing_spend_forecast')}}
+)
 select
     date,
     channel,
-    coalesce(spend, 0) as spend
-from {{ref('marketing_spend')}}
+    coalesce(spend, 0) as spend,
+    coalesce(spend_forecast, 0) as spend_forecast
+from cte_spend
+natural full join cte_forecast
 order by date, channel
