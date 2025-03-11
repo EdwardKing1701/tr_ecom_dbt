f895bf40f47a8729bd2db053147c109015b4335e
Edward King <ed.king@truereligion.com>
2025-03-11 10:25:49 -0400

    remove spend_forecast from fct_spend_by_channel

--- models/analysis/daily/fct_spend_by_channel.sql
+++ models/analysis/daily/fct_spend_by_channel.sql
@@ -7,7 +7,6 @@
 select
     date,
     channel,
-    coalesce(spend, 0) as spend,
-    coalesce(spend_forecast, 0) as spend_forecast
+    coalesce(spend, 0) as spend
 from {{ref('marketing_spend')}}
 order by date, channel
