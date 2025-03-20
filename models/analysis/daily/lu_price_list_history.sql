a73fab785a8f35fd46c3e77052a243ec5c0583ee
Edward King <ed.king@truereligion.com>
2025-03-20 13:16:46 -0400

    update lu_price_list_history

--- models/analysis/daily/lu_price_list_history.sql
+++ models/analysis/daily/lu_price_list_history.sql
@@ -1,12 +1,12 @@
 {{
     config(
         materialized = 'table',
-        pk = ['sty_id', 'color_id', 'price_list']
+        pk = ['style', 'color', 'price_list']
     )
 }}
 select
-    split(style_color, '-')[0] as sty_id,
-    split(style_color, '-')[1] as color_id,
+    split(style_color, '-')[0] as style,
+    split(style_color, '-')[1] as color,
     replace(source_file_name, '.xlsx', '') as price_list,
     effective_date as price_from_date,
     end_date as price_to_date,
@@ -20,4 +20,4 @@ from {{source('load', 'price_lists')}}
 join {{ref('stg_price_list')}} using(source_file_name)
 where
     price_list_type = 'Standard'
-order by price_from_date, price_list, sty_id, color_id
+order by price_from_date, price_list, style, color
