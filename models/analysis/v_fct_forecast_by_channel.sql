d6965867cfce8044af27e2485a362cccc8b40b08
Edward King <ed.king@truereligion.com>
2025-04-06 16:46:03 -0400

    update v_fct_forecast_by_channel

--- models/analysis/v_fct_forecast_by_channel.sql
+++ models/analysis/v_fct_forecast_by_channel.sql
@@ -13,15 +13,12 @@ cte_last_updated as (
 )
 select
     day_key as date,
-    case
-        when channel = 'Push' then 'Mobile Push Notifications'
-        when channel = 'Social' then 'Organic Social'
-        else channel
-    end as channel,
+    coalesce(channel_correction, channel) as channel,
     f_web_fcst_co_ord_rtl as forecast,
     f_web_bdgt_co_ord_rtl as budget,
     rcd_upd_ts as source_synced_ts
 from {{source('robling_tr', 'dwh_f_web_pln_d_b')}}
 natural join cte_last_updated
+left join {{ref('channel_correction')}} using (channel)
 where
     channel <> 'ECOM Total'
