ff3aa88d154d595f8f9f8a93678038901acb6cf1
Automaton <nobody@invalid>
2025-04-11 06:35:23 -0700

    Automated update of seed files

--- seed_files_single.bat
+++ seed_files_single.bat
@@ -1,7 +1,7 @@
 cd C:\Users\edwar\Documents\dbt_auto
-git add seeds/*
-git commit -m "Automated update of seed files"
 git fetch
 git rebase
+git add seeds/*
+git commit -m "Automated update of seed files"
 git push
 dbt seed --select %1 --target prod
