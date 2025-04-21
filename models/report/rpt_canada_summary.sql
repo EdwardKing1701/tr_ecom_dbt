84aeefb89463a8f99141e4cf16ff728468420f39
Edward King <ed.king@truereligion.com>
2025-04-21 10:49:16 -0400

    add rpt_canada_summary

---
+++ models/report/rpt_canada_summary.sql
@@ -0,0 +1,44 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+with
+cte_orders as (
+    select
+        id as order_id,
+        case
+            when id like 'FL%' then
+                to_timestamp_tz(creation_date::timestamp_ntz)
+            else
+                convert_timezone('America/Los_Angeles', creation_date)
+        end as order_ts,
+        order_ts::date as order_date,
+        c_flow_experience_id as region
+from {{source('sfcc', 'orders_history')}}
+where
+    order_id like 'FL%'
+qualify row_number() over (partition by id order by _fivetran_synced desc) = 1
+),
+cte_sales as (
+    select
+        order_date as date,
+        count(distinct order_id) as orders,
+        sum(quantity) as sale_qty,
+        sum(price_after_order_discount) as sale_amt
+    from {{source('sfcc', 'order_product_item')}}
+    join cte_orders using(order_id)
+    where
+        region = 'canada'
+    group by all
+),
+cte_traffic as (
+    select
+        date,
+        sessions
+    from {{ref('ga_canada')}}
+)
+select
+    *
+from cte_sales
+natural full join cte_traffic
