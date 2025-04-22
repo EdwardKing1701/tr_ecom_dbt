e963f8203a2738164696a4d7f948dedc117aefbb
Edward King <ed.king@truereligion.com>
2025-04-22 15:13:16 -0400

    add sources_classes_per_division

---
+++ tests/sources/sources_classes_per_division.sql
@@ -0,0 +1,19 @@
+with
+cte_test as (
+    select
+        count(distinct division) as data
+    from {{ref('v_dim_item')}}
+)
+select
+    '{{this.name}}' as test_name,
+    'analysis.v_dim_item' as source_name,
+    null as dimension,
+    'More than 5 divisions exist' as description,
+    null as source_synced_ts,
+    null as max_data_date,
+    analysis.local_time(current_timestamp()) as tested_ts,
+    data,
+    null as stop_execution,
+    data <= 5 as passed
+from cte_test
+where not passed
