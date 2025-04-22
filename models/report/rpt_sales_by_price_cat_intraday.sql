218940ab2e9bcec8ba2ddcf78bb97fd8332a6151
Edward King <ed.king@truereligion.com>
2025-04-22 11:22:44 -0400

    add rpt_sales_by_price_cat_intraday

---
+++ models/report/rpt_sales_by_price_cat_intraday.sql
@@ -0,0 +1,50 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+with
+cte_catalogue as (
+    select
+        sku as product_id,
+        color,
+        division,
+        class
+    from {{ref('v_dim_item')}}
+),
+cte_orders as (
+    select
+        id as order_id,
+        case
+            when id like 'FL%' then
+                to_timestamp_tz(creation_date::timestamp_ntz)
+            else
+                convert_timezone('America/Los_Angeles', creation_date)
+        end as order_ts,
+        order_ts::date as order_date
+from {{source('sfcc', 'orders_history')}}
+where
+    (
+        order_date between current_date() - 1 and current_date()
+        or order_date in (select date_ly from {{ref('dim_date')}} where date between current_date() - 1 and current_date())
+    )
+    and order_ts <= current_timestamp()
+qualify row_number() over (partition by id order by _fivetran_synced desc) = 1
+)
+select
+    order_date as date,
+    hour(order_ts) as hour,
+    division,
+    class,
+    price_list,
+    coalesce(price_category, 'REG') as price_category,
+    sum(quantity) as sale_qty,
+    sum(price_after_order_discount) as sale_amt
+from {{source('sfcc', 'order_product_item')}}
+join cte_orders using (order_id)
+left join cte_catalogue using (product_id)
+left join {{ref('lu_price_list_history')}} using (color)
+where
+    coalesce(effective_date, '1901-01-01') <= order_date
+    and coalesce(end_date, '2199-01-01') >= order_date
+group by all
