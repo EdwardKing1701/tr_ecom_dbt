577c7bae4e703606737a6d94542373a162f75b6c
Edward King <ed.king@truereligion.com>
2025-04-24 14:06:45 -0400

    add sources_shipping_freshness

---
+++ tests/sources/sources_tax_freshness.sql
@@ -0,0 +1,21 @@
+with
+cte_test as (
+    select
+        coalesce(sum(tax), 0) as data
+    from {{ref('v_fct_orders')}}
+    where
+        date = current_date() - 1
+)
+select
+    '{{this.name}}' as test_name,
+    'analysis.v_fct_orders' as source_name,
+    null as dimension,
+    'Tax > 100' as description,
+    null as source_synced_ts,
+    null as max_data_date,
+    analysis.local_time(current_timestamp()) as tested_ts,
+    data,
+    null as stop_execution,
+    data > 100 as passed
+from cte_test
+where not passed
