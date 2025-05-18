d0b949ca0c27fdfa9fb9103d45b08341aa042d54
Edward King <ed.king@truereligion.com>
2025-05-18 17:27:45 -0400

    add dim_customer

--- models/analysis/daily/dim_customer.sql
+++ models/analysis/daily/dim_customer.sql
@@ -1,16 +1,39 @@
 {{
     config(
         materialized = 'table',
-        pk = ['customer_id']
+        pk = ['email_address']
     )
 }}
+with
+cte_email_address as (
+    select
+        trim(lower(sfcc_customer_id)) as customer_id,
+        analysis.email_address(email_address) as email_address,
+        current_timestamp() as inserted_ts
+    from {{source('robling_sftp_v', 'v_dwh_f_customers')}}
+    where
+        sfcc_customer_id is not null
+        and email_address is not null
+    qualify row_number() over (partition by trim(lower(sfcc_customer_id)) order by email_address) = 1
+),
+cte_orders as (
+    select
+        meas_dt as date,
+        trim(lower(attr_col_7)) as customer_id,
+        attr_col_2 as order_id,
+        attr_col_11 as order_type,
+        f_meas_rtl as sale_amt
+    from {{source('robling_merch', 'dv_dm_f_meas_il_b')}}
+    where
+        meas_cde = 'CO_ORDERED'
+)
 select
-    trim(lower(sfcc_customer_id)) as customer_id,
-    analysis.email_address(email_address) as email_address,
-    current_timestamp() as inserted_ts
-from {{source('robling_sftp_v', 'v_dwh_f_customers')}}
-where
-    sfcc_customer_id is not null
-    and email_address is not null
-qualify row_number() over (partition by trim(lower(sfcc_customer_id)) order by email_address) = 1
-order by customer_id
+    email_address,
+    array_agg(distinct customer_id) as customer_ids,
+    min(date) as first_order_date,
+    max(date) as last_order_date,
+    sum(sale_amt) as sale_amt_ltd
+from cte_orders
+join cte_email_address using(customer_id)
+group by all
+order by email_address
