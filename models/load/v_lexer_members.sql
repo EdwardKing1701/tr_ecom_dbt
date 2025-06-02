40ba509709bd61732191bacccb07ff46ab4f22a4
Edward King <ed.king@truereligion.com>
2025-06-02 17:18:59 -0400

    add v_lexer_members

---
+++ models/load/v_lexer_members.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+select
+    customer_id::varchar as customer_id,
+    nullif(email, '') as email,
+    nullif(user_id, '') as user_id,
+    nullif(gender, '') as gender,
+    nullif(inferred_gender, '') as inferred_gender,
+    nullif(generation, '') as generation,
+    is_a_customer,
+    first_order_date,
+    record,
+    nullif(listrak_record, '')::boolean as listrak_record,
+    nullif(communication_opt_in, '')::boolean as communication_opt_in,
+    nullif(opt_in, '') as opt_in,
+    nullif(opt_in_status, '') as opt_in_status,
+    try_to_timestamp_ntz(subscribe_date___master_list) as subscribe_date___master_list,
+    try_to_timestamp_ntz(unsubscribe_date___master_list) as unsubscribe_date___master_list,
+    nullif(sms_opt_in_status, '') as sms_opt_in_status,
+    try_to_timestamp_ntz(sms_subscribe_date) as sms_subscribe_date,
+    try_to_timestamp_ntz(sms_unsubscribe_date) as sms_unsubscribe_date,
+    try_to_timestamp_ntz(app_first_open_date) as app_first_open_date,
+    nullif(push_opt_in, '') as push_opt_in
+from {{source('load', 'lexer_members')}}
