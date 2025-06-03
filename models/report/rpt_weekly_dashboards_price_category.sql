b5c49d2adca24167ffe9a19949b17597f4943487
Edward King <ed.king@truereligion.com>
2025-06-03 15:46:47 -0400

    update update

--- models/report/rpt_weekly_dashboards_price_category.sql
+++ models/report/rpt_weekly_dashboards_price_category.sql
@@ -59,7 +59,7 @@ cte_calendar as (
     where
         to_date_type = 'TODAY'
         and time_period in ('TY', 'LY')
-        and year_id >= (select year_id - 2 from {{ref('dim_date')}} where date = previous_day(current_date(), 'sa'))
+        and year_id >= (select year_id - 1 from {{ref('dim_date')}} where date = previous_day(current_date(), 'sa'))
         and date <= previous_day(current_date(), 'sa')
 )
 select
