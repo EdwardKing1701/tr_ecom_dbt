e3c931c1f85c192035fec1077a08598c4951a918
Patrick Gault <p.gault@truereligion.com>
2025-06-05 21:31:14 -0700

    updated all lexer fields to reflect input

--- models/load/v_lexer_members.sql
+++ models/load/v_lexer_members.sql
@@ -5,8 +5,8 @@
 }}
 select
     customer_id::varchar as lexer_customer_id,
-    nullif(email, '') as email,
-    nullif(user_id, '') as user_id,
+    nullif(email, '') as email_address,
+    nullif(user_id, '') as yotopo_user_id,
     nullif(gender, '') as gender,
     nullif(inferred_gender, '') as inferred_gender,
     nullif(generation, '') as generation,
@@ -16,9 +16,9 @@ select
     nullif(listrak_record, '')::boolean as listrak_record,
     nullif(communication_opt_in, '')::boolean as communication_opt_in,
     nullif(opt_in, '') as listrak_opt_in,
-    nullif(opt_in_status, '') as opt_in_status,
-    try_to_timestamp_ntz(subscribe_date___master_list) as subscribe_date___master_list,
-    try_to_timestamp_ntz(unsubscribe_date___master_list) as unsubscribe_date___master_list,
+    nullif(opt_in_status, '') as listrak_opt_in_status,
+    try_to_timestamp_ntz(subscribe_date___master_list) as listrak_subscribe_date_master_list,
+    try_to_timestamp_ntz(unsubscribe_date___master_list) as listrak_unsubscribe_date_master_list,
     nullif(sms_opt_in_status, '') as sms_opt_in_status,
     try_to_timestamp_ntz(sms_subscribe_date) as sms_subscribe_date,
     try_to_timestamp_ntz(sms_unsubscribe_date) as sms_unsubscribe_date,
