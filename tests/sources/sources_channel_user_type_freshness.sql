6b2abbb76ffe6a5a4e45ef49f5ecb66470f55b00
Edward King <ed.king@truereligion.com>
2025-06-25 12:34:54 -0400

    add sources_channel_user_type_freshness

---
+++ tests/sources/sources_channel_user_type_freshness.sql
@@ -0,0 +1,23 @@
+with
+cte_test as (
+    select
+        date as dimension,
+        coalesce(sum(sessions), 0) as data
+    from {{ref('ga_channels_user_type')}}
+    where
+        date >= current_date() - 7
+    group by all
+)
+select
+    '{{this.name}}' as test_name,
+    'load.ga_channels_user_type' as source_name,
+    dimension,
+    'Channel sessions > 100,000' as description,
+    null as source_synced_ts,
+    null as max_data_date,
+    analysis.local_time(current_timestamp()) as tested_ts,
+    data,
+    null as stop_execution,
+    data > 100000 as passed
+from cte_test
+where not passed
