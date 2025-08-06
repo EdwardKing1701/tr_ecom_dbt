d3c179ce4546d6c6b0115453d7f9d8fff53886d0
Edward King <ed.king@truereligion.com>
2025-08-06 17:10:10 -0400

    add rpt_ecom_promotion_intraday

---
+++ models/report/rpt_ecom_promotion_intraday.sql
@@ -0,0 +1,142 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+with
+cte_calendar as (
+    select
+        date,
+        xfrm_date as demand_date,
+        time_period
+    from {{ref('date_xfrm')}}
+    where
+        to_date_type = 'TODAY'
+        and time_period in ('TY', 'LY')
+        and date = current_date()
+),
+cte_order_items as (
+    select
+        order_item_id,
+        order_id,
+        date,
+        time_period,
+        order_type,
+        sale_qty * base_price as base_price,
+        sale_qty,
+        sale_cost,
+        sale_amt,
+        order_discount,
+        item_discount
+    from {{ref('stg_sfcc_orders')}}
+    join cte_calendar using(demand_date)
+    where
+        hour(demand_ts) < hour(current_timestamp())
+),
+cte_orders as (
+    select
+        order_id,
+        date,
+        time_period,
+        order_type,
+        sum(base_price) as base_price,
+        sum(sale_qty) as sale_qty,
+        sum(sale_cost) as sale_cost,
+        sum(sale_amt) as sale_amt,
+        sum(order_discount) as order_discount,
+        sum(item_discount) as item_discount,
+    from cte_order_items
+    group by all
+),
+cte_item_promos as (
+    select
+        item_id as order_item_id,
+        order_id,
+        price_adjustment_type,
+        promotion_id,
+        campaign_id,
+        abs(net_price) as promo_discount
+    from {{source('sfcc', 'price_adjustment')}}
+    where
+        price_adjustment_type = 'PRODUCT_ITEM'
+),
+cte_order_promos as (
+    select
+        order_id,
+        price_adjustment_type,
+        promotion_id,
+        campaign_id,
+        abs(net_price) as promo_discount
+    from {{source('sfcc', 'price_adjustment')}}
+    where
+        price_adjustment_type = 'ORDER'
+),
+cte_all_promos as (
+    select
+        order_id,
+        'ALL' as price_adjustment_type,
+        listagg(distinct promotion_id, ',') within group (order by promotion_id) as promotion_id,
+        listagg(distinct campaign_id, ',') within group (order by campaign_id) as campaign_id,
+        sum(abs(net_price)) as promo_discount
+    from {{source('sfcc', 'price_adjustment')}}
+    where
+        price_adjustment_type in ('PRODUCT_ITEM', 'ORDER')
+    group by all
+)
+-- All promotions
+select
+    date,
+    time_period,
+    order_type,
+    coalesce(promotion_id, 'No promotion') as promotion_id,
+    coalesce(campaign_id, 'No promotion') as campaign_id,
+    'ALL' as price_adjustment_type,
+    count(distinct order_id) as orders,
+    sum(base_price) as base_price,
+    sum(sale_qty) as sale_qty,
+    sum(sale_cost) as sale_cost,
+    sum(sale_amt) as sale_amt,
+    sum(promo_discount) as promo_discount
+from cte_orders
+left join cte_all_promos using (order_id)
+group by all
+
+union all
+
+-- Order promotions
+select
+    date,
+    time_period,
+    order_type,
+    promotion_id,
+    campaign_id,
+    price_adjustment_type,
+    count(distinct order_id) as orders,
+    sum(base_price) as base_price,
+    sum(sale_qty) as sale_qty,
+    sum(sale_cost) as sale_cost,
+    sum(sale_amt) as sale_amt,
+    sum(promo_discount) as promo_discount
+from cte_orders
+join cte_order_promos using (order_id)
+group by all
+
+union all
+
+-- Item promotions
+select
+    date,
+    time_period,
+    order_type,
+    promotion_id,
+    campaign_id,
+    price_adjustment_type,
+    count(distinct order_id) as orders,
+    sum(base_price) as base_price,
+    sum(sale_qty) as sale_qty,
+    sum(sale_cost) as sale_cost,
+    sum(sale_amt) as sale_amt,
+    sum(promo_discount) as promo_discount
+from cte_order_items
+join cte_item_promos using (order_id, order_item_id)
+group by all
