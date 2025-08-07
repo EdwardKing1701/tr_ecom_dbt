2e4c1292b1bdd6f0b3d70f083ea09979bfbf01ee
Edward King <ed.king@truereligion.com>
2025-08-07 10:45:18 -0400

    update fivetran_attentive_campaign

--- models/load/fivetran_attentive_campaign.sql
+++ models/load/fivetran_attentive_campaign.sql
@@ -5,18 +5,34 @@
 }}
 select
     try_to_date(date) as date,
-    message_id,
-    message_name,
-    message_segments,
-    sends,
-    sms_send_cost,
-    carrier_fees,
-    total,
     _file,
-    _sheet_name,
     _line,
+    _sheet_name,
     _modified,
-    _fivetran_synced
-from {{source('attentive_sftp', 'campaign')}}
-where
-    date <> 'Total'
+    _fivetran_synced,
+    campaign_mms_segments,
+    campaign_video_send_cost,
+    automated_mms_segments,
+    characters,
+    campaign_total,
+    sends,
+    message_segments,
+    campaign_sms_send_cost,
+    automated_carrier_fees,
+    automated_sms_segments,
+    campaign_carrier_fees,
+    message_name,
+    campaign_sms_segments,
+    automated_sms_send_cost,
+    image_url,
+    message_type,
+    campaign_video_segments,
+    type,
+    has_video,
+    automated_mms_send_cost,
+    automated_total,
+    message_copy,
+    campaign_mms_send_cost,
+    message_id,
+    total_spend
+from {{source('attentive_sftp', 'campaign_detail')}}
