6e39cad5c129d129909d7f4135f4c5e8ca7b0b99
Edward King <ed.king@truereligion.com>
2025-08-20 11:30:34 -0400

    update sources_division_count

--- tests/sources/sources_division_count.sql
+++ tests/sources/sources_division_count.sql
@@ -8,12 +8,12 @@ select
     '{{this.name}}' as test_name,
     'analysis.dim_item' as source_name,
     null as dimension,
-    'More than 5 divisions exist' as description,
+    'More than 6 divisions exist' as description,
     null as source_synced_ts,
     null as max_data_date,
     analysis.local_time(current_timestamp()) as tested_ts,
     data,
     null as stop_execution,
-    data <= 5 as passed
+    data <= 6 as passed
 from cte_test
 where not passed
