84bb8fae3a0a0c00b219d34d9c1c527a821c2425
Edward King <ed.king@truereligion.com>
2025-09-02 19:06:27 -0400

    add stg_robling_sfcc_customer

--- models/analysis/dim_customer.sql
+++ models/analysis/dim_customer.sql
@@ -7,14 +7,9 @@
 with
 cte_email_address as (
     select
-        trim(lower(sfcc_customer_id)) as customer_id,
-        analysis.email_address(email_address) as email_address,
-        current_timestamp() as inserted_ts
-    from {{source('robling_sftp_v', 'v_dwh_f_customers')}}
-    where
-        sfcc_customer_id is not null
-        and email_address is not null
-    qualify row_number() over (partition by trim(lower(sfcc_customer_id)) order by email_address) = 1
+        customer_id,
+        email_address
+    from {{ref('stg_robling_sfcc_customer')}}
 ),
 cte_orders as (
     select
---
+++ models/stg/stg_robling_sfcc_customer.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized = 'table',
+        pk = ['customer_id']
+    )
+}}
+select
+    trim(lower(sfcc_customer_id)) as customer_id,
+    analysis.email_address(email_address) as email_address
+from {{source('robling_sftp_v', 'v_dwh_f_customers')}}
+where
+    sfcc_customer_id is not null
+    and email_address is not null
+qualify row_number() over (partition by trim(lower(sfcc_customer_id)) order by email_address) = 1
